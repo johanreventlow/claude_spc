3da18dc68b165012adc49dc4ef4337f32efd3f16
johanreventlow <johanreventlow@users.noreply.github.com>
2025-08-22 14:16:06 +0200

    fejlfinding - overlay 'Ingen graf endnu Indtast data i tabellen til venstre eller upload en fil'

--- R/modules/github_sync.R
+++ R/modules/github_sync.R
@@ -6,5 +6,5 @@ library(gert)
 # git_status()
 
 git_add(".")
-git_commit_all("fejlfinding - 'genererer graf'")
+git_commit_all("fejlfinding - overlay 'Ingen graf endnu Indtast data i tabellen til venstre eller upload en fil'")
 git_push()
